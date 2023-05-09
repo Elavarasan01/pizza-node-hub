42c8c08e59635b9839d7b72f0e9fec79fa2b3298
Elavarasan Senthilkumar <elavarasan.senthil@peopletech.com>
2023-05-09 14:47:08 +0530

    node js files are addeded

---
+++ controller/PizzaController.js
@@ -0,0 +1,127 @@
+const Pizza=require("../models/menu")
+const mongoose=require("mongoose")
+
+//getting all pizza details
+const getPizza=(req,res,next)=>{
+    Pizza.find()
+    .then((response)=>{
+        res.json({
+          response
+        })
+    })
+    .catch((err)=>{
+        res.json({
+            message:"Error Occured!"
+        })
+    })
+}
+
+//getting single pizza details
+const getPizzaById=(req,res,next)=>{
+    let pizzaId=req.body.pizzaId;
+    Pizza.findById(pizzaId)
+    .then((response)=>{
+        res.json({
+            response
+        })
+    })
+    .catch((err)=>{
+        res.json({
+            message:"Error Occured"
+        })
+    })
+}
+
+//post a new pizza
+const postPizza = (req, res, next) => {
+    let pizza = new Pizza({
+        pizzaname: req.body.pizzaname,
+        price: req.body.price,
+        phone: req.body.phone,
+        address: req.body.address
+    });
+    
+    pizza.save()
+        .then(response => {
+            res.json({
+                message: "Pizza added successfully!"
+            });
+        })
+        .catch(err => {
+            res.json({
+                message: "Error occurred"
+            });
+        });
+};
+
+//update the pizza details
+const updatePizza = (req, res, next) => {
+    const pizzaId = req.body.pizzaId;
+    const update = {
+      pizzaname: req.body.pizzaname,
+      price: req.body.price,
+      phone: req.body.phone,
+      address: req.body.address,
+    };
+  
+    if (!mongoose.Types.ObjectId.isValid(pizzaId)) {
+      return res.status(400).json({ message: "Invalid pizza id" });
+    }
+  
+    Pizza.findByIdAndUpdate(pizzaId, { $set: update }, { new: true })
+      .then((response) => {
+        if (!response) {
+          return res.status(404).json({ message: "Pizza not found" });
+        }
+        res.json({ message: "Updated successfully!" });
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).json({ message: "Server error" });
+      });
+  };
+
+//delete the pizza
+const deletePizza = (req, res, next) => {
+    const pizzaId = req.body.pizzaId;
+    if (!mongoose.Types.ObjectId.isValid(pizzaId)) {
+        return res.status(400).json({ message: 'Invalid pizza ID' });
+    }
+    Pizza.findByIdAndDelete(pizzaId)
+        .then((response) => {
+            if (!response) {
+                return res.status(404).json({ message: 'Pizza not found' });
+            }
+            res.json({
+                message: 'Deleted!'
+            });
+        })
+        .catch((err) => {
+            res.status(500).json({
+                message: 'Error Occurred'
+            });
+        });
+};
+
+const searchPizza = (req, res, next) => {
+    const query = req.query.q;
+  
+    Pizza.find({ pizzaname: { $regex: query, $options: "i" } })
+      .then((pizzas) => {
+        res.json(pizzas);
+      })
+      .catch((err) => {
+        res.json({
+          message: "Error occurred",
+        });
+      });
+  };
+
+module.exports={
+    getPizza,
+    getPizzaById,
+    postPizza,
+    updatePizza,
+    deletePizza,
+    searchPizza
+};
---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const PizzaRoute = require("./routes/routes");
+
+const app = express();
+
+mongoose.connect('mongodb+srv://kselaa2010:Elavarasan@cluster0.mp3dumz.mongodb.net/pizzadb', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("connected to db");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const port = process.env.PORT || 2000;
+app.get("/hello", (req, res) => {
+    res.send('Welcome to node backend');
+})
+app.use("/", PizzaRoute);
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+module.exports = app;
