293eecc5f8b52c544e7f07288841d21f959a91a7
Elavarasan Senthilkumar <elavarasan.senthil@peopletech.com>
2023-05-10 13:01:31 +0530

    new changes

---
+++ dist/models/menu.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const PizzaSchema = new Schema({
+  pizzaname: {
+    type: String
+  },
+  price: {
+    type: Number
+  },
+  phone: {
+    type: String
+  },
+  address: {
+    type: String
+  }
+}, { timestamps: true });
+
+const Pizza = mongoose.model("Pizza", PizzaSchema);
+module.exports = Pizza;
