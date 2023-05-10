6701e9b3688446678cd276bc323350a9b10ddec3
Elavarasan Senthilkumar <elavarasan.senthil@peopletech.com>
2023-05-10 15:31:07 +0530

    dist folder changes

---
+++ dist/routes/routes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const router = express.Router();
+
+const Pizza = require("../controller/PizzaController");
+
+router.get("/", Pizza.getPizza);
+router.post("/get", Pizza.getPizzaById);
+router.post("/create", Pizza.postPizza);
+router.post("/modify", Pizza.updatePizza);
+router.post("/remove", Pizza.deletePizza);
+router.post("/search", Pizza.searchPizza);
+
+module.exports = router;
