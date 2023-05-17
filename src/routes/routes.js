6f4c9041618e2852bf324d23988000af5acbfd94
Elavarasan Senthilkumar <elavarasan.senthil@peopletech.com>
2023-05-17 15:51:07 +0530

    local array and mongo both are seperated and given

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,14 +1,14 @@
-const express=require("express")
+const express = require("express");
 
-const router=express.Router();
+const router = express.Router();
 
-const Pizza=require("../controller/PizzaController")
+const Pizza = require("../controller/PizzaController");
 
-router.get("/",Pizza.getPizza);
-router.post("/get",Pizza.getPizzaById);
-router.post("/create",Pizza.postPizza);
-router.post("/modify",Pizza.updatePizza);
-router.post("/remove",Pizza.deletePizza);
-router.post("/search",Pizza.searchPizza);
+router.get("/", Pizza.getPizza);
+router.post("/get", Pizza.getPizzaById);
+router.post("/create", Pizza.postPizza);
+router.post("/modify", Pizza.updatePizza);
+router.post("/remove", Pizza.deletePizza);
+router.post("/search", Pizza.searchPizza);
 
 module.exports = router;
--- src/server.js
+++ src/server.js
@@ -1,23 +1,18 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors=require("cors")
 
 const app = express();
-// const router = express.Router();
 
-const PizzaController=require('./controller/PizzaController')
+const PizzaRoute = require("./routes/routes");
+const Pages = require("./routes/pages");
 
-app.get("/",PizzaController.getPizza)
-app.post("/get",PizzaController.getPizzaById)
-app.post("/create",PizzaController.postPizza)
-app.post("/modify",PizzaController.updatePizza)
-app.post("/remove",PizzaController.deletePizza)
-app.post("/search",PizzaController.searchPizza)
-
-// const PizzaRoute = require("./routes/routes");
-
-
- mongoose.connect('mongodb+srv://kselaa2010:Elavarasan@cluster0.mp3dumz.mongodb.net/pizzadb', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose
+  .connect(
+    "mongodb+srv://kselaa2010:Elavarasan@cluster0.mp3dumz.mongodb.net/pizzadb",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
   .then(() => {
     console.log("connected to db");
   })
@@ -30,17 +25,19 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const port = process.env.PORT || 2000;
 app.get("/node", (req, res) => {
-    res.send('Welcome to node backend');
-})
+  res.send("Welcome to node backend");
+});
 
 app.get("/serve", (req, res) => {
-    res.send('Welcome to nodejs backend serveices');
-})
-
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+  res.send("Welcome to nodejs backend serveices");
 });
-// app.use("/api/pizza", PizzaRoute);
 
-// module.exports = router;
+app.use(cors())
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+app.use("/api/pizza", PizzaRoute);
+app.use("/",Pages);
+
 module.exports = app;
